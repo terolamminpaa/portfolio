f06342623af945a1e0b4002556b76ef03bd76a06
Tero Lamminpaa <tero.lamminpaa@gmail.com>
2021-12-07 13:29:54 +0200

    Updated firebase config

--- src/api/firebase-config.ts
+++ src/api/firebase-config.ts
@@ -1,14 +1,15 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID",
+  apiKey: "AIzaSyCIo4F7qu5Hk51FdX9b0JcLzA3Ab-ewJsg",
+  authDomain: "portfolio-tero-lamminpaa.firebaseapp.com",
+  projectId: "portfolio-tero-lamminpaa",
+  storageBucket: "portfolio-tero-lamminpaa.appspot.com",
+  messagingSenderId: "503351430121",
+  appId: "1:503351430121:web:b8af708048828022235978",
+  measurementId: "G-0TX5GL8VKP"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
